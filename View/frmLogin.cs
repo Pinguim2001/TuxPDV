74b6eaea75cd35804a17d6cdf67da27f336582ad
Alysson Bonacina Dornelles <alysson.dornelles@orquidea.com.br>
2025-02-21 17:03:01 -0300

    Feito

--- View/frmLogin.cs
+++ View/frmLogin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -43,6 +44,11 @@ namespace TuxPDV.View
             }
 
             TipoUsuario type = usuService.getTypeUser(login);
+
+            if(type == TipoUsuario.ADMIN)
+            {
+
+            }
         }
     }
 }
